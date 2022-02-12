6c70ea5a0dd288cd8099aa03df31019d03163bf8
Vasim <vpathan88@gmail.com>
2022-02-12 22:53:41 +0530

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router/index'
+import 'bootstrap/dist/css/bootstrap.css'
+import '@/assets/css/style.css'
+import 'bootstrap/dist/js/bootstrap.bundle'
+
+
+createApp(App).mount('#app')
